91a2d7c077e825e486343aef83f47b53a8e07094
Nick Andresen <nimandresen@gmail.com>
2022-08-24 09:36:41 -0400

    Add decision-/world-event controllers

---
+++ SpaceRaceKai/Server/Controllers/DecisionEventController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceRaceKai.Server.Services.DecisionEvent;
+using SpaceRaceKai.Shared.Models.DecisionEvent;
+
+namespace SpaceRaceKai.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DecisionEventController : ControllerBase
+    {
+        private readonly IDecisionEventService _service;
+
+        public DecisionEventController(IDecisionEventService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<List<DecisionEventListItem>> Index()
+        {
+            var decisionEvents = await _service.GetAllDecisionEventsAsync();
+            return decisionEvents.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> DecisionEvent(int id)
+        {
+            var decisionEvent = await _service.GetDecisionEventByIdAsync(id);
+            return (decisionEvent is null) ? NotFound() : Ok(decisionEvent);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(DecisionEventCreate model)
+        {
+            if (model is null) return BadRequest();
+            else return (await _service.CreateDecisionEventAsync(model)) ? Ok() : UnprocessableEntity();
+        }
+
+        [HttpPut("edit/{id}")]
+        public async Task<IActionResult> Edit(int id, DecisionEventEdit model)
+        {
+            if (model is null || !ModelState.IsValid) return BadRequest();
+            if (model.Id != id) return BadRequest();
+            return (await _service.UpdateDecisionEventAsync(model)) ? Ok() : BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var decisionEvent = await _service.GetDecisionEventByIdAsync(id);
+            if (decisionEvent is null) return NotFound();
+            else return (await _service.DeleteDecisionEventAsync(id)) ? Ok() : BadRequest();
+        }
+    }
+}
---
+++ SpaceRaceKai/Server/Controllers/WorldEventController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpaceRaceKai.Server.Services.WorldEvent;
+using SpaceRaceKai.Shared.Models.WorldEvent;
+
+namespace SpaceRaceKai.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorldEventController : ControllerBase
+    {
+        private readonly IWorldEventService _service;
+
+        public WorldEventController(IWorldEventService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<List<WorldEventListItem>> Index()
+        {
+            var worldEvents = await _service.GetAllWorldEventsAsync();
+            return worldEvents.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> WorldEvent(int id)
+        {
+            var worldEvent = await _service.GetWorldEventByIdAsync(id);
+            return (worldEvent is null) ? NotFound() : Ok(worldEvent);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(WorldEventCreate model)
+        {
+            if (model is null) return BadRequest();
+            else return (await _service.CreateWorldEventAsync(model)) ? Ok() : UnprocessableEntity();
+        }
+
+        [HttpPut("edit/{id}")]
+        public async Task<IActionResult> Edit(int id, WorldEventEdit model)
+        {
+            if (model is null || !ModelState.IsValid) return BadRequest();
+            if (model.Id != id) return BadRequest();
+            return (await _service.UpdateWorldEventAsync(model)) ? Ok() : BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var worldEvent = await _service.GetWorldEventByIdAsync(id);
+            if (worldEvent is null) return NotFound();
+            else return (await _service.DeleteWorldEventAsync(id)) ? Ok() : BadRequest();
+        }
+    }
+}
